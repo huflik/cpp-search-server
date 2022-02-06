f4ce0f23eddcc00b4ea0cb2dd9b6b4c8096da3c2
Olga Fedorova <oflik@ya.ru>
2022-02-06 09:36:01 +0300

    Спринт 4. Итоговый проект

---
+++ search-server/request_queue.cpp
@@ -0,0 +1,25 @@
+#include "request_queue.h"
+
+using namespace std;
+
+RequestQueue::RequestQueue(const SearchServer& search_server) :
+        search_server_(search_server),
+        count_empty_requests_(0),
+        current_time_(0)
+    {
+    }
+
+    vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentStatus status) {
+        // напишите реализацию
+        return AddFindRequest(raw_query, [status](int document_id, DocumentStatus document_status, int rating) {
+            return document_status == status;
+            });
+    }
+
+    vector<Document> RequestQueue::AddFindRequest(const string& raw_query) {
+        return AddFindRequest(raw_query, DocumentStatus::ACTUAL);
+    }
+
+    int RequestQueue::GetNoResultRequests() const {
+        return count_empty_requests_;
+    }
---
+++ search-server/request_queue.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "search_server.h"
+
+#include <deque>
+
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server);
+    // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
+    template <typename DocumentPredicate>
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate); 
+
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+
+    std::vector<Document> AddFindRequest(const std::string& raw_query);
+
+    int GetNoResultRequests() const;
+
+private:
+    struct QueryResult {
+        bool empty_request_;
+        uint64_t timestamp;
+    };
+    std::deque<QueryResult> requests_;
+    const static int min_in_day_ = 1440;
+    const SearchServer& search_server_;
+    int count_empty_requests_;
+    uint64_t current_time_;
+};
+// сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    std::vector<Document> request = search_server_.FindTopDocuments(raw_query, document_predicate);
+    ++current_time_;
+    if (request.empty()) {
+        ++count_empty_requests_;
+        requests_.push_back({ true, current_time_ });
+    }
+    else {
+        requests_.push_back({ false, current_time_ });
+    }
+    // pop old request
+    if (min_in_day_ <= current_time_ - requests_.front().timestamp) {
+        if (requests_.front().empty_request_) {
+            --count_empty_requests_;
+        }
+        requests_.pop_front();
+    }
+    return request;
+}
